de0f91e9a5cd6f3957b512ab9ae02a5b6588277a
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-15 15:37:03 +0100

    Add descriptions to tf variables

--- tools/csw_cloudfront_distribution/variables.tf
+++ tools/csw_cloudfront_distribution/variables.tf
@@ -2,12 +2,16 @@ variable "region" {
   default = "eu-west-2"
 }
 variable "dns_zone_fqdn" {
-  default = "csw.staging.gds-cyber-security.digital"
+  description = "Fully qualified parent domain name"
 }
+
 variable "env" {
   default = "test"
 }
 variable "sub_domain" {
+  description = "Name of the desired DNS Record"
   default = "test"
 }
-variable "api_gateway_url" {}
+variable "api_gateway_url" {
+  description = "The default URL created by APIGAteway"
+}
