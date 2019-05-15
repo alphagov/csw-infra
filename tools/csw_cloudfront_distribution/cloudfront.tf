f9b5e08187efebda31cdf760d33ab8f3f64175c2
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-15 13:50:54 +0100

    Correct https protocols property syntax

--- tools/csw_cloudfront_distribution/cloudfront.tf
+++ tools/csw_cloudfront_distribution/cloudfront.tf
@@ -7,7 +7,7 @@ resource "aws_cloudfront_distribution" "cf_distribution" {
       http_port = 80
       https_port = 443
       origin_protocol_policy = "https-only"
-      origin_ssl_protocols = "TLSv1.2"
+      origin_ssl_protocols = ["TLSv1.2"]
     }
   }
 
