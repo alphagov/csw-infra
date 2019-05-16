4b9ffb5e7033a62bed032b11c01d8d3d4ee5bb34
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-16 16:54:43 +0100

    Add us-east-1 secondary provider to create cert

--- tools/csw_cloudfront_distribution/certificate.tf
+++ tools/csw_cloudfront_distribution/certificate.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "cf_cert" {
-  region            = "us-east-1"
+  provider          = "use1"
   domain_name       = "${local.target_url}"
   validation_method = "DNS"
 
