64c3dcdc8abd167c9a1812abb6b29744483cbff7
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-16 17:09:50 +0100

    Temporarily remove failing loop counter

--- tools/csw_cloudfront_distribution/certificate.tf
+++ tools/csw_cloudfront_distribution/certificate.tf
@@ -20,15 +20,32 @@ resource_record_name - The name of the DNS record to create to validate the cert
 resource_record_type - The type of DNS record to create
 resource_record_value - The value the DNS record needs to have
 */
-
+output "domain_validation_options" {
+  value = "${aws_acm_certificate.cf_cert.domain_validation_options}"
+}
+/*
 resource "aws_route53_record" "cert_validation_record" {
-  count   = "${length(aws_acm_certificate.cf_cert.domain_validation_options)}"
-  zone_id = "${data.terraform_remote_state.dns_zone.zone_id}"
-  name    = "${lookup(aws_acm_certificate.cf_cert.domain_validation_options[count.index], "resource_record_name")}"
-  type    = "${lookup(aws_acm_certificate.cf_cert.domain_validation_options[count.index], "resource_record_type")}"
-  ttl     = "3600"
+  depends_on  = ["aws_acm_certificate.cf_cert"]
+  count       = "${length(aws_acm_certificate.cf_cert.domain_validation_options)}"
+  zone_id     = "${data.terraform_remote_state.dns_zone.zone_id}"
+  name        = "${lookup(aws_acm_certificate.cf_cert.domain_validation_options[count.index], "resource_record_name")}"
+  type        = "${lookup(aws_acm_certificate.cf_cert.domain_validation_options[count.index], "resource_record_type")}"
+  ttl         = "3600"
 
   records = [
     "${lookup(aws_acm_certificate.cf_cert.domain_validation_options[count.index], "resource_record_value")}"
   ]
 }
+*/
+resource "aws_route53_record" "cert_validation_record" {
+  depends_on  = ["aws_acm_certificate.cf_cert"]
+  zone_id     = "${data.terraform_remote_state.dns_zone.zone_id}"
+  name        = "${lookup(aws_acm_certificate.cf_cert.domain_validation_options[0], "resource_record_name")}"
+  type        = "${lookup(aws_acm_certificate.cf_cert.domain_validation_options[0], "resource_record_type")}"
+  ttl         = "3600"
+
+  records = [
+    "${lookup(aws_acm_certificate.cf_cert.domain_validation_options[0], "resource_record_value")}"
+  ]
+}
+
