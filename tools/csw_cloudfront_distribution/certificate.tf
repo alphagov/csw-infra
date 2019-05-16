4bf19c12cc5484e702a72485ffaa89f4647ff330
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-16 16:19:50 +0100

    Create a DNS validation record for the certificate

--- tools/csw_cloudfront_distribution/certificate.tf
+++ tools/csw_cloudfront_distribution/certificate.tf
@@ -10,3 +10,22 @@ resource "aws_acm_certificate" "cf_cert" {
     create_before_destroy = true
   }
 }
+
+/*
+exports
+domain_validation_options
+domain_name - The domain to be validated
+resource_record_name - The name of the DNS record to create to validate the certificate
+resource_record_type - The type of DNS record to create
+resource_record_value - The value the DNS record needs to have
+*/
+
+resource "aws_route53_record" "cert_validation_record" {
+  zone_id = "${data.terraform_remote_state.dns_zone.zone_id}"
+  name    = "${aws_acm_certificate.cf_cert.domain_validation_options.resource_record_name}"
+  type    = "${aws_acm_certificate.cf_cert.domain_validation_options.resource_record_type}"
+
+  records = [
+    "${aws_acm_certificate.cf_cert.domain_validation_options.resource_record_value}"
+  ]
+}
