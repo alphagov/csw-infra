32337665ecb6276ab34f79b6ada745a17ffd9db9
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-16 15:50:22 +0100

    Point dns record at cloudfront distribution alias

--- tools/csw_cloudfront_distribution/dns.tf
+++ tools/csw_cloudfront_distribution/dns.tf
@@ -1,10 +1,11 @@
 resource "aws_route53_record" "csw_cname" {
   zone_id = "${data.terraform_remote_state.dns_zone.zone_id}"
   name    = "${var.sub_domain}"
-  type    = "CNAME"
-  ttl     = "30"
+  type    = "A"
 
-  records = [
-    "${var.api_gateway_url}"
-  ]
+  alias {
+    name                   = "${aws_cloudfront_distribution.cf_distribution.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.cf_distribution.hosted_zone_id}"
+    evaluate_target_health = true
+  }
 }
