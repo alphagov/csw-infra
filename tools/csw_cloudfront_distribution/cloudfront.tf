9a1ddbb82425e5507aae0f29d8c4417aa6c425f0
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2019-05-16 16:24:54 +0100

    Specify that cf distribution depends on the dns certificate validation record

--- tools/csw_cloudfront_distribution/cloudfront.tf
+++ tools/csw_cloudfront_distribution/cloudfront.tf
@@ -6,6 +6,7 @@ locals {
 }
 
 resource "aws_cloudfront_distribution" "cf_distribution" {
+  depends_on = ["aws_route53_record.cert_validation_record"]
   origin {
     domain_name = "${local.api_gateway_domain}"
     origin_id   = "${local.cf_origin_id}"
